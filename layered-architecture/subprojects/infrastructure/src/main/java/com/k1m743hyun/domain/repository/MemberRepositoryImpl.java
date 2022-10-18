05ab7adf6308c5c7d94acfe08ae9fa06b9586d52
Taehyun Kim <k1m743hyun@icloud.com>
2022-10-18 11:20:47 +0900

    Update the Infrastructure layer

--- layered-architecture/subprojects/infrastructure/src/main/java/com/k1m743hyun/domain/repository/MemberRepositoryImpl.java
+++ layered-architecture/subprojects/infrastructure/src/main/java/com/k1m743hyun/domain/repository/MemberRepositoryImpl.java
@@ -3,7 +3,6 @@ package com.k1m743hyun.domain.repository;
 import com.k1m743hyun.data.entity.Member;
 import com.k1m743hyun.provider.jpa.MemberJpaRepository;
 import com.k1m743hyun.repository.MemberRepository;
-import java.util.List;
 import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
@@ -14,23 +13,34 @@ public class MemberRepositoryImpl implements MemberRepository {
 
     private final MemberJpaRepository memberJpaRepository;
 
-//    @Override
-//    public Member save(Member member) {
-//        return memberJpaRepository.save(member);
-//    }
-
     @Override
-    public Optional<Member> findById(Long id) {
-        return memberJpaRepository.findById(id);
+    public Optional<Member> getMember(Member member) {
+
+        if (member.getId() != null) {
+            return memberJpaRepository.findById(member.getId());
+        } else {
+            return memberJpaRepository.findByName(member.getName());
+        }
     }
 
     @Override
-    public Optional<Member> findByName(String name) {
-        return memberJpaRepository.findByName(name);
+    public Optional<Member> saveMember(Member member) {
+
+        memberJpaRepository.save(member);
+
+        return Optional.of(member);
     }
 
-//    @Override
-//    public List<Member> findAll() {
-//        return memberJpaRepository.findAll();
-//    }
+    @Override
+    public Optional<Member> editMember(Member member) {
+        return memberJpaRepository.findById(member.getId());
+    }
+
+    @Override
+    public Optional<Member> deleteMember(Member member) {
+
+        memberJpaRepository.deleteById(member.getId());
+
+        return Optional.of(member);
+    }
 }
--- layered-architecture/subprojects/infrastructure/src/main/java/com/k1m743hyun/provider/jpa/MemberJpaRepository.java
+++ layered-architecture/subprojects/infrastructure/src/main/java/com/k1m743hyun/provider/jpa/MemberJpaRepository.java
@@ -1,22 +1,18 @@
 package com.k1m743hyun.provider.jpa;
 
 import com.k1m743hyun.data.entity.Member;
-import com.k1m743hyun.repository.MemberRepository;
-import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MemberJpaRepository extends JpaRepository<Member, Long> {
 
-//    @Override
-//    Member save(Member member);
-
-    @Override
     Optional<Member> findById(Long id);
 
-    //@Override
     Optional<Member> findByName(String name);
 
-//    @Override
-//    List<Member> findAll();
+    //@Override
+    //Optional<Member> save(Member member);
+
+    @Override
+    void deleteById(Long id);
 }
