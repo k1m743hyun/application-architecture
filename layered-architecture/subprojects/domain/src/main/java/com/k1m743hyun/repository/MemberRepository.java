bd91d812a46201abec7f6d07b7ad2f5189f879f1
Taehyun Kim <k1m743hyun@icloud.com>
2022-10-18 10:38:57 +0900

    Update the MemberRepository.java

--- layered-architecture/subprojects/domain/src/main/java/com/k1m743hyun/repository/MemberRepository.java
+++ layered-architecture/subprojects/domain/src/main/java/com/k1m743hyun/repository/MemberRepository.java
@@ -1,16 +1,15 @@
 package com.k1m743hyun.repository;
 
 import com.k1m743hyun.data.entity.Member;
-import java.util.List;
 import java.util.Optional;
 
 public interface MemberRepository {
 
-    //Member save(Member member);
+    Optional<Member> getMember(Member member);
 
-    Optional<Member> findById(Long id);
+    Optional<Member> saveMember(Member member);
 
-    Optional<Member> findByName(String name);
+    Optional<Member> editMember(Member member);
 
-    //List<Member> findAll();
+    Optional<Member> deleteMember(Member member);
 }
