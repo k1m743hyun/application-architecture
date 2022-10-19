81244a5f7fb426c3dd113acbec55f4f123cad3a0
Taehyun Kim <k1m743hyun@icloud.com>
2022-10-20 03:33:10 +0900

    Update the project

--- layered-architecture/subprojects/domain/src/main/generated/com/k1m743hyun/data/mapper/MemberMapperImpl.java
+++ layered-architecture/subprojects/domain/src/main/generated/com/k1m743hyun/data/mapper/MemberMapperImpl.java
@@ -1,19 +1,35 @@
 package com.k1m743hyun.data.mapper;
 
+import com.k1m743hyun.data.dto.request.MemberRequestDto;
 import com.k1m743hyun.data.dto.response.MemberResponseDto;
 import com.k1m743hyun.data.dto.response.MemberResponseDto.MemberResponseDtoBuilder;
 import com.k1m743hyun.data.entity.Member;
+import com.k1m743hyun.data.entity.Member.MemberBuilder;
 import java.util.Optional;
 import javax.annotation.processing.Generated;
 import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2022-10-16T18:50:23+0900",
+    date = "2022-10-20T03:05:03+0900",
     comments = "version: 1.4.2.Final, compiler: javac, environment: Java 11.0.15.1 (Oracle Corporation)"
 )
 @Component
 public class MemberMapperImpl implements MemberMapper {
+
+    @Override
+    public Member toEntity(MemberRequestDto requestDto) {
+        if ( requestDto == null ) {
+            return null;
+        }
+
+        MemberBuilder member = Member.builder();
+
+        member.id( requestDto.getId() );
+        member.name( requestDto.getName() );
+
+        return member.build();
+    }
 
     @Override
     public MemberResponseDto toDto(Optional<Member> request) {
