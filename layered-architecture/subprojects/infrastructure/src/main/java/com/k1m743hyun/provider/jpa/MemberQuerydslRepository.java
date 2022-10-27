2d644a459150d40ea02a3f1c599f4cf12fa9e472
Taehyun Kim <k1m743hyun@icloud.com>
2022-10-27 23:08:04 +0900

    Update the project

--- layered-architecture/subprojects/infrastructure/src/main/java/com/k1m743hyun/provider/jpa/MemberQuerydslRepository.java
+++ layered-architecture/subprojects/infrastructure/src/main/java/com/k1m743hyun/provider/jpa/MemberQuerydslRepository.java
@@ -5,8 +5,6 @@ import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
-import static com.k1m743hyun.data.entity.QMember.member;
-
 @RequiredArgsConstructor
 @Repository
 public class MemberQuerydslRepository {
@@ -14,18 +12,14 @@ public class MemberQuerydslRepository {
     private final JPAQueryFactory queryFactory;
 
     public Member getMember(Long id) {
-        return queryFactory.selectFrom(member).where(member.id);
     }
 
     public Member saveMember() {
-        return queryFactory;
     }
 
     public Member editMember() {
-        return queryFactory.update(member).set();
     }
 
     public Member deleteMember() {
-        return queryFactory.delete(member).where();
     }
 }
