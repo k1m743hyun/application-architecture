52cc031c8c1b207226514a09f888eee5782ba140
Taehyun Kim <k1m743hyun@icloud.com>
2022-11-07 10:29:27 +0900

    Create subprojects/presentation/src/test/java/com/k1m743hyun/validation/marker/MemberRequestValidationTest.java for Test Coverage

---
+++ layered-architecture/subprojects/presentation/src/test/java/com/k1m743hyun/validation/marker/MemberRequestValidationTest.java
@@ -0,0 +1,12 @@
+package com.k1m743hyun.validation.marker;
+
+
+import org.junit.jupiter.api.Test;
+
+class MemberRequestValidationTest {
+
+    @Test
+    void test() {
+        new MemberRequestValidation();
+    }
+}
